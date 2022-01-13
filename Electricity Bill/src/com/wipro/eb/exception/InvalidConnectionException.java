859e2f86c405b29a02716077561ac5b0fd783290
user <user@DESKTOP-D0NFV9L>
2022-01-13 09:36:28 +0530

    My First Project

---
+++ Electricity Bill/src/com/wipro/eb/exception/InvalidConnectionException.java
@@ -0,0 +1,12 @@
+package com.wipro.eb.exception;
+
+public class InvalidConnectionException extends Exception {
+
+    public String toString() {
+   	 
+   	 return "Invalid ConnectionType";
+    }
+    
+    
+}
+
